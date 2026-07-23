fa207483132cf631178e4ef625ac4ea121e4d1e2
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-23 12:59:00 +0700

    Add Arrival of the General [A] from Codeforces

---
+++ 144/A - Arrival of the General/solution.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cctype>
+ 
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;  cin >> n;
+    int max_value = 0, min_value = 10e7, min_i = -1, max_i = -1;
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        if (x <= min_value){
+            min_i = i;
+            min_value = x;
+        }
+        if (x > max_value){
+            max_i = i;
+            max_value = x;
+        }
+    }
+    if (min_i < max_i)      cout << n - min_i + max_i - 2;
+    else    cout << max_i + n - 1 - min_i;
+    return 0;
+}
