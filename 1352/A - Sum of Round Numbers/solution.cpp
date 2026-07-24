2129914313be5d9639ddfca9d151496754b5a620
Twilight_ <dominhhoangd24ptit@gmail.com>
2026-07-24 12:16:50 +0700

    Add Sum of Round Numbers [A] from Codeforces

--- 1352/A - Sum of Round Numbers/solution.cpp
+++ 1352/A - Sum of Round Numbers/solution.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<stack>
+#include<vector>
  
 using namespace std;
  
@@ -9,22 +9,19 @@ int main(){
     int testcase;
     cin >> testcase;
     while(testcase--){
-        stack<int> ans;
+        vector<int> ans;
         int n, pow = 1;
         cin >> n;
         for (int i = 0; i < 5; i++){
             if (n  == 0)    break;
             int x = n % 10;
             n /= 10;
-            if (x != 0)     ans.push(pow * x);
+            if (x != 0)     ans.push_back(pow * x);
             pow *= 10;
         }
         cout << ans.size() << '
 ';
-        while (!ans.empty()){
-            cout << ans.top() << " ";
-            ans.pop();
-        }
+        for (int i : ans)       cout << i << " ";
         cout << '
 ';
     }
